f7a8025d51a321d53ea848c7667da62c782dd9d0
AlexisToanen <alexistoanen@gmail.com>
2019-04-20 11:24:03 +0200

    peacock version 1.0

---
+++ peacock.js
@@ -0,0 +1,40 @@
+var app = {
+    init: function(){
+
+        window.addEventListener('scroll', app.launchScrollAppear);
+    },
+    launchScrollAppear: function(){
+    
+        var peacockTopBottom = document.querySelectorAll('.peacock-frm-top, .peacock-frm-bottom');
+        var peacockLeftRight = document.querySelectorAll('.peacock-frm-left, .peacock-frm-right');
+
+        for (index = 0; index < peacockTopBottom.length; index +=1) {
+            app.scrollAppear(peacockTopBottom[index], 'y');
+        }
+
+        for (index = 0; index < peacockLeftRight.length; index +=1) {
+            app.scrollAppear(peacockLeftRight[index], 'x');
+        }
+
+    },
+    scrollAppear: function(domElement, direction){
+   
+        var peacockPositionTopBottom = domElement.getBoundingClientRect().top;
+        var screenPositionTopBottom = window.innerHeight / 2;
+
+        var peacockPositionLeftRight = domElement.getBoundingClientRect().top;
+        var screenPositionLeftRight = window.innerHeight / 2;
+        
+        if(peacockPositionTopBottom < screenPositionTopBottom && direction == 'y'){
+            domElement.classList.add('peacock-appear-' + direction);
+        }
+
+        if(peacockPositionLeftRight < screenPositionLeftRight && direction == 'x'){
+            domElement.classList.add('peacock-appear-' + direction);
+        }
+        
+    },
+
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
